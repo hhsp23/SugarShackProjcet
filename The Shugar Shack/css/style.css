5f7689e04bebc2db763cda6ada5a707826b0d843
hhsp23 <hari.hadzispiroski@gmail.com>
2024-05-18 22:53:55 +0200

    Add files via upload

---
+++ The Shugar Shack/css/style.css
@@ -0,0 +1,355 @@
+@font-face {
+  font-family: cabinet grotesk;
+  src: url(../Font/CabinetGrotesk-Variable.ttf)
+}
+
+/* Basic styles */
+body {
+  font-family: cabinet grotesk;
+  margin: 0;
+  padding: 0;
+  margin: 0 !important;
+  padding: 0 !important;
+
+
+}
+
+/* Text styles */
+
+h1,
+h2,
+h3,
+h4,
+h5 {
+  color: black;
+  font-weight: 900;
+  text-transform: uppercase;
+  margin: 0;
+}
+
+h1 {
+  font-size: 6rem;
+  line-height: 1;
+}
+
+h2 {
+  font-size: 3rem;
+  line-height: 1.2;
+}
+
+h3 {
+  font-size: 2.5rem;
+  line-height: 1.2;
+}
+
+h4 {
+  font-size: 2rem;
+  line-height: 1.3;
+}
+
+h5 {
+  font-size: 1.5rem;
+  line-height: 1.4;
+}
+
+p {
+  font-size: 1.125rem;
+  line-height: 1.5rem;
+  color: black;
+  margin: 0;
+
+
+
+
+}
+
+/* Button styles */
+
+button {
+  padding: 0.75rem 1.5rem;
+  font-size: 1rem;
+  line-height: 1.5;
+
+
+
+}
+
+.primary-btn {
+  cursor: pointer;
+  border: none;
+  border-radius: 15px;
+  background-color: #FF7B9E;
+  color: black;
+  transition: background-color 0.3s;
+  font-weight: 700;
+}
+
+.secondary-btn {
+  cursor: pointer;
+  border-width: 1px;
+  border-radius: 15px;
+  background-color: transparent;
+  color: black;
+  transition: background-color 0.3s;
+  font-weight: 500;
+}
+
+/* Navbar style */
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+  padding: 8px 64px;
+  background-color: transparent;
+
+}
+
+.navbar a {
+  text-decoration: none;
+  color: black;
+  font-size: 1rem;
+  font-weight: 400;
+}
+
+
+.nav-links {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+}
+
+/* Section styles */
+
+section {
+  padding: 112px 64px;}
+
+.hero {
+  background-size: cover;
+  background-position: center;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+  height: auto;
+  overflow-x: hidden;
+  padding: 0;
+}
+
+.hero-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.bg {
+  position: relative;
+  margin: auto;
+  
+}
+
+/* Hero Section styles */
+
+.hero-content-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  min-height: 100svh;
+  max-height: 60rem;
+  justify-content: center
+}
+
+.hero-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  justify-content: center;
+  text-align: center;
+}
+
+.hero-button-group {
+  display: flex;
+  padding-top: 16px;
+  align-items: flex-start;
+  gap: 16px;
+}
+
+/* Prodcut Section styles */
+
+.product-section-container {
+  display: flex;
+flex-direction: column;
+gap: 80px;
+}
+
+.product-content {
+  display: flex;
+justify-content: space-between;
+align-items: flex-end;
+align-self: stretch;
+}
+
+.product-content-text {
+  display: flex;
+flex-direction: column;
+align-items: flex-start;
+gap: 16px;
+
+}
+
+.product-grid {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 48px;
+  align-self: stretch;
+}
+
+.product-list-container {
+  display: flex;
+align-items: flex-start;
+gap: 48px;
+align-self: stretch;
+width: auto;
+}
+
+.product-button-group {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 16px;
+}
+
+.product-carousel{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 48px;
+  align-self: stretch;
+
+}
+
+
+/* Prodcut card styles */
+
+
+.placeholder-image-icon {
+  align-self: stretch;
+  position: relative;
+  border-radius: 40px;
+  max-width: 100%;
+  overflow: hidden;
+  height: 486px;
+  flex-shrink: 0;
+  object-fit: cover;
+  background-color: #FFF6EE;
+  }
+  .product-name {
+  align-self: stretch;
+  position: relative;
+  font-weight: 600;
+  font-size: 1.125rem;
+  font-weight: 600;
+  }
+  .product-description {
+  align-self: stretch;
+  position: relative;
+  font-weight: 400;
+  font-size: 0.875rem;
+  }
+  .info-header {
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  }
+  .price {
+  align-self: stretch;
+  position: relative;
+  font-size: 1.25rem;
+  font-weight: 600;
+  }
+  .product-info {
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 8px;
+  }
+  .product {
+  width:400px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 16px;
+  text-align: left;
+  color: #000;
+  
+  }
+
+
+  /* Prodcut nav styles */
+  .dot {
+    width: 8px;
+    position: relative;
+    border-radius: 50%;
+    background-color: #000;
+    height: 8px;
+    }
+    .dot1 {
+    width: 8px;
+    position: relative;
+    border-radius: 50%;
+    background-color: #ccc;
+    height: 8px;
+    }
+    .slider-dots {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 8px;
+    }
+    .icon {
+    width: 24px;
+    position: relative;
+    height: 24px;
+    overflow: hidden;
+    flex-shrink: 0;
+    }
+    .button {
+    border-radius: 50px;
+    border: 1px solid #000;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 12px;
+    }
+    .slider-buttons {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 15px;
+    }
+    .card-nav {
+    width: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    }
